1950979f701a0e3dd39e89811f12f44a23855c49
Olena Kolesnik <lelya6350@gmail.com>
2026-03-04 23:15:00 +0200

    made loader error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,12 +4,18 @@ import { toast, Toaster } from "react-hot-toast";
 import axios from "axios";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
   const [movies, setMovies] = useState([]);
   const handleSubmit = async (query: string) => {
     try {
+      setIsLoading(true);
+      setIsError(false);
       setMovies([]);
       const { data } = await axios.get("https://api.themoviedb.org/3/search/movie", {
         params: {
@@ -25,14 +31,18 @@ function App() {
       }
       setMovies(data.results);
     } catch (error) {
-      toast.error("Something went wrong.");
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
     <>
       <Toaster position="top-right" />
       <SearchBar onSubmit={handleSubmit} />
-      
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
       {movies.length > 0 && <MovieGrid movies={movies} />}
     </>
   );
