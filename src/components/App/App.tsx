962e423c02414cfca0e6881876abe9d474b54725
Olena Kolesnik <lelya6350@gmail.com>
2026-03-04 14:42:37 +0200

    made task1

---
+++ src/components/App/App.tsx
@@ -0,0 +1,41 @@
+
+import { useState } from "react";
+import { toast, Toaster } from "react-hot-toast";
+import axios from "axios";
+import SearchBar from "../SearchBar/SearchBar";
+const TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const handleSubmit = async (query: string) => {
+    try {
+      setMovies([]);
+      const { data } = await axios.get("https://api.themoviedb.org/3/search/movie", {
+        params: {
+          query: query,
+        },
+        headers: {
+          Authorization: `Bearer ${TOKEN}`,
+        },
+      });
+      if (data.results.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+      setMovies(data.results);
+    } catch (error) {
+      toast.error("Something went wrong.");
+    }
+  };
+  return (
+    <>
+      <Toaster position="top-right" />
+      <SearchBar onSubmit={handleSubmit} />
+      {movies.map((movie: any) => (
+        <div key={movie.id}>{movie.title}</div>
+      ))}
+    </>
+  );
+}
+
+export default App;
