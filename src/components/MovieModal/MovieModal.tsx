8611d39f241fe43d720d88167806d5d3b51e4e32
Olena Kolesnik <lelya6350@gmail.com>
2026-03-05 21:14:57 +0200

    made hw3

--- src/components/MovieModal/MovieModal.tsx
+++ src/components/MovieModal/MovieModal.tsx
@@ -1,38 +1,62 @@
 import css from "./MovieModal.module.css";
 import { createPortal } from "react-dom";
 import type { Movie } from "../../types/movie";
+import { useEffect } from "react";
 
 interface MovieModalProps {
-    movie: Movie;
-    onClose: () => void;
+  movie: Movie;
+  onClose: () => void;
 }
 
 function MovieModal({ movie, onClose }: MovieModalProps) {
-    return createPortal(
-        <div className={css.backdrop} role="dialog" aria-modal="true">
-            <div className={css.modal}>
-                <button className={css.closeButton} aria-label="Close modal"
-                onClick={onClose }>
-                    &times;
-                </button>
-                <img
-                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-                    alt={movie.title}
-                    className={css.image}
-                />
-                <div className={css.content}>
-                    <h2>{movie.title}</h2>
-                    <p>{movie.overview}</p>
-                    <p>
-                        <strong>Release Date:</strong> {movie.release_date}
-                    </p>
-                    <p>
-                        <strong>Rating:</strong> {movie.vote_average}/10
-                    </p>
-                </div>
-            </div>
-        </div>,
-        document.body
-    );
+  useEffect(() => {
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+      document.addEventListener("keydown", handleKeydown);
+      document.body.style.overflow = "hidden";
+    return () => {
+        document.removeEventListener("keydown", handleKeydown);
+        document.body.style.overflow = "";
+    };
+  }, [onClose]);
+    const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target === event.currentTarget) {
+            onClose();
+        }
+    };
+
+  return createPortal(
+      <div className={css.backdrop} role="dialog" aria-modal="true"
+      onClick={handleBackdropClick}>
+      <div className={css.modal}>
+        <button
+          className={css.closeButton}
+          aria-label="Close modal"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          className={css.image}
+        />
+        <div className={css.content}>
+          <h2>{movie.title}</h2>
+          <p>{movie.overview}</p>
+          <p>
+            <strong>Release Date:</strong> {movie.release_date}
+          </p>
+          <p>
+            <strong>Rating:</strong> {movie.vote_average}/10
+          </p>
+        </div>
+      </div>
+    </div>,
+    document.body,
+  );
 }
 export default MovieModal;
